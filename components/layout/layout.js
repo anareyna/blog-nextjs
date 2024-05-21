9d177fa51563510dafac89c810695197b7dbf377
Ana Reyna <anareymej@gmail.com>
2024-05-21 20:22:44 +0200

    Add analytics

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,13 @@
 import React from "react";
 import MainNavigation from "./main-navigation";
 import classes from "./layout.module.scss";
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 export default function Layout(props) {
 	return (
 		<>
 			<MainNavigation />
 			<main className={classes.main}>{props.children}</main>
+			<GoogleAnalytics gaId="G-XC5N03N66H" />
 		</>
 	);
 }
